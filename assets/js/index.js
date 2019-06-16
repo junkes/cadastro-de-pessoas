cdf70334f1b8df4bda1f9d4b8bd5ce3560896d0c
Julio Junkes <junkes.julio@gmail.com>
2019-06-15 23:50:55 -0300

    primeiro commit

---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+let socket = io('http://192.168.1.100:3000')
+socket.on('connect', () => {
+    if (typeof app != 'undefined' && app.pessoas.length) app.pessoas = []
+    status_conexao.innerHTML = 'conectado!'
+    status_conexao.style.backgroundColor = 'green'
+    status_conexao.style.padding = '0 5px'
+    status_conexao.style.color = 'white'
+    console.log('conectou!')
+})
+socket.on('disconnect', () => {
+    status_conexao.innerHTML = 'desconectado!'
+    status_conexao.style.backgroundColor = 'red'
+    status_conexao.style.padding = '0 5px'
+    status_conexao.style.color = 'white'
+    console.log('desconectou!')
+})
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        pessoas: []
+    },
+    methods: {
+        cadastrarPessoa: function () {
+            if (nome.value && telefone.value) {
+                let pessoa = {
+                    nome: nome.value,
+                    telefone: telefone.value
+                }
+                socket.emit('cadastro', JSON.stringify(pessoa))
+                nome.value = telefone.value = ''
+            } else {
+                alert('Preencha todos os campos!')
+            }
+        },
+        addPessoa: function (pessoa) {
+            this.pessoas.push({
+                id: pessoa.id,
+                nome: pessoa.nome,
+                telefone: pessoa.telefone,
+                data_cadastro: moment(pessoa.data_cadastro).format('DD/MM/YYYY HH:mm:ss')
+            })
+        },
+        deletePessoa: function (id) {
+            socket.emit('delete', id)
+        }
+    }
+})
+
+socket.on('cadastros', data => {
+    app.pessoas = []
+    let pessoas = JSON.parse(data)
+    pessoas.forEach(pessoa => {
+        app.addPessoa(pessoa)
+    })
+})
+
+socket.on('novo cadastro', data => {
+    let pessoa = JSON.parse(data)
+    app.addPessoa(pessoa)
+})
---
+++ index.js
@@ -0,0 +1,74 @@
+/**
+ * Servidor NodeJS rodando websocket.io
+ */
+
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const mysql  = require('mysql2/promise')
+
+let pessoas = []
+
+const connect = async () => {
+    return await mysql.createConnection({
+        host: 'localhost',
+        user: 'websocket',
+        password: 'websocket',
+        database: 'websocket_teste'
+    });
+}
+
+const query = async (connection, sql) => {
+    return await connection.execute(sql);
+}
+
+const getPessoas = async () => {
+    const connection = await connect()
+    const [rows] = await query(connection, 'SELECT * FROM pessoas')
+    connection.destroy()
+    return rows
+}
+
+const setPessoas = async pessoa => {
+    const connection = await connect()
+    const sql = `INSERT INTO pessoas (nome, telefone) values ('${pessoa.nome}','${pessoa.telefone}')`
+    const result = await query(connection,sql); 
+    connection.destroy()
+    return result[0].insertId
+} 
+
+const deletePessoa = async id => {
+    const connection = await connect()
+    const sql = `DELETE FROM pessoas WHERE id = ${id}`
+    await query(connection,sql);
+    connection.destroy()
+}
+
+io.on('connection', async socket => {
+
+    pessoas = await getPessoas()
+    
+    socket.emit('cadastros',JSON.stringify(pessoas))
+    
+    socket.on('disconnect', () => { /*console.log(socket.id)*/ })
+    
+    socket.on('cadastro', async pessoa => {
+        pessoaObj = JSON.parse(pessoa)
+        pessoaObj.id = await setPessoas(pessoaObj)
+        pessoa = JSON.stringify(pessoaObj)
+        console.log('pessoa cadastrada: ' + pessoa)
+        socket.emit('novo cadastro', pessoa )
+        socket.broadcast.emit('novo cadastro', pessoa )
+    } )
+
+    socket.on('delete', async id => {
+        console.log('deletando pessoa: ' + id)
+        await deletePessoa(id)
+        pessoas = await getPessoas()
+        socket.emit('cadastros', JSON.stringify(pessoas))
+        socket.broadcast.emit('cadastros', JSON.stringify(pessoas))
+    })
+    
+})
+server.listen(3000)
